7437ae03b497cc6bf5855bdd8eb2e44c59da02f5
nitinkelwa <nitinkelwa0@gmail.com>
2026-04-08 12:10:45 +0530

    8/4/26

--- Core Python/module/module2.py
+++ Core Python/module/module2.py
@@ -2,3 +2,6 @@ import module1
 
 print(module1.sum(10,20))
 print(module1.sub(60,10))
+
+
+
---
+++ Core Python/oop/encaptulation.py
@@ -0,0 +1,54 @@
+class Automobile:
+    def __init__(self):
+        print("Details of my car  Mahendra scorpio ")
+
+        self.__model = None
+        self.__colour = None
+        self.__speed = 0
+        self.__gear = 0
+
+        # setter getter for model
+
+    def get_model(self):
+        return self.__model
+
+    def set_model(self, model):
+        self.__model = model
+
+        # setter and getter for colour
+
+    def get_colour(self):
+        return self.__colour
+
+    def set_colour(self, colour):
+        self.__colour = colour
+
+    # setter and getter for  speed
+    def get_speed(self):
+        return self.__speed
+
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    # setter and getter for gear
+    def get_gear(self):
+        return self.__gear
+
+    def set_gear(self, gear):
+        self.__gear = gear
+
+    # object creation
+
+
+car = Automobile()
+car.set_model(" Mahendra scorpio s11")
+car.set_colour(" Dark Black")
+car.set_speed("160/170 km/h")
+car.set_gear("5+Reverse ")
+
+print("car model: ", car.get_model())
+print("car colour:", car.get_colour())
+print("car speed: ", car.get_speed())
+print("No of gear in car: ",car.get_gear())
+
+
